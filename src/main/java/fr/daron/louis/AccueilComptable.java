b47ef014c097c8ca06903ebb6a36912bb3e0f672
RMasart <raphael.masart@gmail.com>
2024-03-22 16:43:24 +0100

    compta

---
+++ src/main/java/fr/daron/louis/AccueilComptable.java
@@ -0,0 +1,13 @@
+package fr.daron.louis;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AccueilComptable {
+    @FXML
+    private Button btnAccueilC;
+
+    @FXML
+    private Button btnHistComp;
+
+}
