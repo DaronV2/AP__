df128a62d7c77af925aa8443820902a2b567c5b1
Louis <darkenciel31@gmail.com>
2024-03-25 14:08:40 +0100

    Fin de la page de création de fiche de remboursement visiteur

--- src/main/java/fr/daron/louis/SecondaryController.java
+++ src/main/java/fr/daron/louis/SecondaryController.java
@@ -2,7 +2,6 @@ package fr.daron.louis;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
@@ -15,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class SecondaryController {
 
@@ -47,12 +47,6 @@ public class SecondaryController {
     @FXML
     private DatePicker barreMois;
 
-    /*public void setMois(DatePicker mois) {
-        String dateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDateTime.now());
-        mois.setText("00-00-0000");
-        mois.setText(dateTime.toString());
-    }*/
-
     @FXML
     private TextField montantUnitaireKm;
 
@@ -81,12 +75,14 @@ public class SecondaryController {
 
     @FXML
     private DatePicker barreMois11;
+
+    @FXML
+    private Text resultatRequete1;
     
     @FXML
     void initialize() {
         setMatricule(matricule);
         setNom(nom);
-        //setMois(mois);
 
         matricule.setEditable(false);
         nom.setEditable(false);
@@ -121,27 +117,26 @@ public class SecondaryController {
         String afM1 = afMontant1.getText();
         String afL2 = afLibelle2.getText();
         String afM2 = afMontant2.getText();
+
         LocalDate moisHf1 = barreMois1.getValue();
-        String moisHf1String = moisHf1.toString();
         LocalDate moisHf2 = barreMois11.getValue();
-        LocalDate moisString = barreMois.getValue();
+        LocalDate moisff = barreMois.getValue();
+
         String matriculeString = matricule.getText();
         String px_km = montantUnitaireKm.getText();
 
         Sqldb sql2 = new Sqldb();
-        // Connection c = sql2.connexionDb();
-        // Statement stmnt = c.createStatement();
         Connection c = sql2.connexionDb();
         Statement stmnt = c.createStatement();
 
         java.util.UUID uuid = UUID.randomUUID();
         String uuidString = uuid.toString();
 
-        moisHf1String = moisHf1String.replace("-", ",");
-        moisHf1String = moisHf1String.replace("03", "3");
 
         if(moisHf1!= null & afL1 != null & afM1 != null){
-            moisHf1String = "STR_TO_DATE(\""+moisHf1String+", 10,10,10\", \"%Y,%m,%d %h,%i,%s\")";
+            String moisHf1String = moisHf1.toString();
+            moisHf1String = moisHf1String.replace("-", ",");
+            moisHf1String = "STR_TO_DATE(\""+moisHf1String+"\", \"%Y,%m,%d\")";
             System.out.println(moisHf1String);
             String sql1 = "INSERT INTO hors_forfait ( hf_date, hf_libelle, hf_montant,ff_id) VALUES ("+moisHf1String+",'"+afL1+"',"+afM1+",'"+uuidString+"')";
             stmnt.executeUpdate(sql1);
@@ -151,32 +146,25 @@ public class SecondaryController {
         }
 
         if(moisHf2!= null & afL2 != null & afM2 != null){
-            String sql1 = String.format("INSERT INTO hors_forfait ( hf_date, hf_libelle, hf_montant,ff_id) VALUES (%s,'%s',%s,'%s')",moisHf2, afL1, afM2,uuidString);
+            String moisHf2String = moisHf2.toString();
+            moisHf2String = moisHf2String.replace("-", ",");
+            moisHf2String = "STR_TO_DATE(\""+moisHf2String+"\", \"%Y,%m,%d\")";
+            System.out.println(moisHf2String);
+            String sql1 = "INSERT INTO hors_forfait ( hf_date, hf_libelle, hf_montant,ff_id) VALUES ("+moisHf2String+",'"+afL2+"',"+afM2+",'"+uuidString+"')";
             stmnt.executeUpdate(sql1);
         }else{
             System.out.println("yes");
         }
+       
+        String moisFfString = moisff.toString().replace("-", ",");
+        String date = "STR_TO_DATE(\""+moisFfString+"\", \"%Y,%m,%d %h,%i,%s\")";
 
-        String jointure = String.format("SELECT vi_matricule FROM visiteur WHERE cr_identifiant = '%s'",utilisateur.identfiant);
-        ResultSet resultats43 = stmnt.executeQuery(jointure);
-        resultats43.next();
-        matriculeString = resultats43.getNString("vi_matricule");
-        System.out.println(moisString);
-
-        String date = "STR_TO_DATE(\"2017,8,14 10,40,10\", \"%Y,%m,%d %h,%i,%s\")";
-
-        String sql = "INSERT INTO fiche_frais (ff_mois,ff_qte_nuitees, ff_total_nuitees, ff_qte_repas, ff_total_repas, ff_qte_km,vi_matricule,prix_km) VALUES ("+date+", "+nuit+","+totalNuit+","+repasMid+","+totalRepas+","+km1+", '"+matriculeString+"',"+px_km+")";
-        System.out.println(sql);
+        String sql = "INSERT INTO fiche_frais (ff_mois,ff_qte_nuitees, ff_total_nuitees, ff_qte_repas, ff_total_repas, ff_qte_km,vi_matricule,prix_km,ff_id) VALUES ("+date+", "+nuit+","+totalNuit+","+repasMid+","+totalRepas+","+km1+", '"+matriculeString+"',"+px_km+",'"+uuidString+"')";
         stmnt.executeUpdate(sql);
 
-        String getId =String.format("SELECT ff_id from fiche_frais WHERE ff_mois = %s AND vi_matricule = '%s'",moisString,matriculeString);
-        ResultSet resultatId = stmnt.executeQuery(getId);
-        resultatId.next();
 
 
 
-
-        //Faire d'abord les requetes d'hors forfait et ensuite la fiche frais afin dr'avoir l'id
 
     }
 
@@ -187,6 +175,7 @@ public class SecondaryController {
 
     @FXML
     void switchAccueil(ActionEvent event) throws IOException, SQLException {
+
         App.setRoot("primary");
     }
 }
