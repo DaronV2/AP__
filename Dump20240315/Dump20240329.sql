536139894ca299f5db107cf74cdd865ca45a6a04
Louis <darkenciel31@gmail.com>
2024-03-29 13:52:49 +0100

    reglage3 dump

--- Dump20240315/Dump20240329.sql
+++ Dump20240315/Dump20240329.sql
@@ -21,7 +21,6 @@
 
 CREATE SCHEMA gsb_etudiants;
 
-DROP TABLE IF EXISTS `gsb_etudiants.comptable`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `gsb_etudiants.comptable` (
